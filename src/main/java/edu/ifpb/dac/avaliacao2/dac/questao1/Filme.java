ad7be1c97436d5083d172d57ee86b8a465fb76cc
emanuelbatista <emanuelbatista2011@gmail.com>
2015-05-25 07:47:51 -0300

    adcionando na questão 1 a classe Filme

--- src/main/java/edu/ifpb/dac/avaliacao2/dac/questao1/Filme.java
+++ src/main/java/edu/ifpb/dac/avaliacao2/dac/questao1/Filme.java
@@ -6,5 +6,10 @@ package edu.ifpb.dac.avaliacao2.dac.questao1;
  * @author Emanuel Batista da Silva Filho
  */
 public class Filme {
+    private long codigo;
+    private String titulo;
+    private int ano;
+    private String genero;
+    private String diretor;
 
 }
